97cf0cda1a9510b12e5e83ed070992c75e9144b3
Sean Quevedo <squevedo@csumb.edu>
2018-03-04 06:18:11 +0000

    Turning in Lab3

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -1,24 +1,38 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title> </title>
-<<<<<<< HEAD
+   <head>
+        <title> SilverJack </title>
+    </head>
         <meta charset="utf-8" />
         <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
     </head>
     <style> 
         @import url(css/styles.css);
-        
     </style>
-=======
-    </head>
->>>>>>> d668289e3292c5e8ff74cc140c06f9e08e51de12
     <body>
-        
         <?php 
             include 'functions.php';
             play();
         ?>
-
+        <div>
+             
+          <h1 id="seconds"> Total Seconds =  <?= displayElapsedTime(); ?></h1> 
+          <h1 id="seconds"> Average time per play =  <?= ($_SESSION['totalTime']/$_SESSION['totalGames'])  ?> secs</h1> 
+          <h1 id="games"> Numbers of games play = <?= $_SESSION['totalGames'] ?></h1> 
+            <form>
+         <input type="submit" value="Play" DOCTYPE/>
+     </form>
+        </div>
+        <footer id="footer">
+            <hr>
+             CST336 Internet Programming. 2018 &copy <br />
+            <strong> Disclaimer: </strong>
+            The information in this website is fictitous. It's used for academic purposes.
+            <a href="http://csumb.edu">CSUMB</a>
+            <figure>
+                <img src="img/Monterey.jpg" alt="School Logo" height="100" width="150" />
+            </figure>
+            </hr>
+        </footer>
     </body>
 </html>
