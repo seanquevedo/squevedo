0810919ea55782e84dd04e902e50e9d81202c2b3
Sean Quevedo <squevedo@csumb.edu>
2018-03-25 00:33:21 +0000

    Finishing lab 5

--- labs/lab5/scart/functions.php
+++ labs/lab5/scart/functions.php
@@ -42,33 +42,37 @@
     }
     
     function displayCart() {
+        // If the 'cart' is already created, display its items
         if(isset($_SESSION['cart'])) {
-            echo "<table class = 'table'>";
-            foreach ($_SESSION['cart'] as $item) {
+            echo "<table class='table'>";
+            foreach($_SESSION['cart'] as $item) {
                 $itemName = $item['name'];
+                $itemId = $item['id'];
                 $itemPrice = $item['price'];
                 $itemImage = $item['image'];
-                $itemId = $item['id'];
                 $itemQuant = $item['quantity'];
                 
-                //Display item as table row
+                // Display item as table row
                 echo "<tr>";
-                echo "<td><img src = '$itemImage'></td>";
-                echo "<td><h4>$itemName</h4>,/td>";
-                echo "<td><h4>$itemPrice</h4>,/td>";
-                echo "<td><h4>$itemQuant</h4>,/td>";
                 
+                echo "<td><img src='$itemImage'></td>";
+                echo "<td><h4>$itemName</h4></td>";
+                echo "<td><h4>$$itemPrice</h4></td>";
+                echo "<td><h4>$itemQuant</h4></td>";
                 
-                //Hidden input element containing the item name
-                echo "<form method = 'post'>";
-                echo "<input type = 'hidden' name = 'removeId' value = '$itemId'>";
-                echo "<td><input type = 'text' name = 'update' class = 'form-control' placeHolder = '$itemQuant'></td>";
-                // echo "<td><button class = 'btn btn-danger'>Remove</button></td>";
-                echo "<td><button class = 'btn btn-danger'>Update</button></td>";
+                // Hidden input element containing the item name
+                echo "<form method='post'>";
+                echo "<input type='hidden' name='itemId' value='$itemId'>";
+                echo "<td><input type='text' name='update' class='form-control' placeHolder='$itemQuant'></td>";
+                echo "<td><button class='btn btn-danger'>Update</button></td>";
+                echo "</form>";
+                
+                echo "<form method='post'>";
+                echo "<input type='hidden' name='removeId' value='$itemId'>";
+                echo "<td><button class='btn btn-danger'>Remove</button></td>";
                 echo "</form>";
                 
                 echo "</tr>";
-                
             }
             echo "</table>";
         }
