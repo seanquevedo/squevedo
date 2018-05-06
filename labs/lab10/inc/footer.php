cc9722a518d26ab9d00419c6ac41a0112fc8decb
Sean Quevedo <squevedo@csumb.edu>
2018-05-06 05:34:09 +0000

    Lab10

--- labs/lab10/inc/footer.php
+++ labs/lab10/inc/footer.php
@@ -1,3 +1,4 @@
         <hr>
         <strong>Disclaimer:</strong> All information in this website is fake. <br />
+        <img src = "img/buddy_verified.png" alt="Buddy system sticker" />
         &copy;2018 Sean Quevedo
